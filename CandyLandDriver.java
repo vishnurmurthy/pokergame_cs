f768f5c734b0be69c1cd8f391fd9de9a01283213
vishnurmurthy <vishnurmurthy@gmail.com>
2016-05-17 18:20:14 -0400

    Update CandyLandDriver.java

--- CandyLandDriver.java
+++ CandyLandDriver.java
@@ -1,16 +1,13 @@
-//Torbert, e-mail: mr@torbert.com, website: www.mr.torbert.com
-//version 4.5.2003
-
-   import javax.swing.JFrame;
-   public class CandyLandDriver
+import javax.swing.JFrame;
+public class CandyLandDriver
+{
+   public static void main(String[] args)
    {
-      public static void main(String[] args)
-      {
-         JFrame frame = new JFrame("CandyLand!");
-         frame.setSize(800, 650);
-         frame.setLocation(200, 100);
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.getContentPane().add(new CandyLandPanel());
-         frame.setVisible(true);
-      }
+      JFrame frame = new JFrame("CandyLand!");
+      frame.setSize(800, 800);
+      frame.setLocation(200, 1);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.getContentPane().add(new CandyLandPanel());
+      frame.setVisible(true);
    }
+}
